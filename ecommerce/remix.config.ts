9546434b1c81c10b1fcde49378ca737d6afc2875
Rishi Mishra <114818980+Rishi-Mishra0704@users.noreply.github.com>
2025-07-16 22:17:06 +0530

    Updated

--- ecommerce/remix.config.ts
+++ ecommerce/remix.config.ts
@@ -1,10 +1,15 @@
-// remix.config.ts
 import type { AppConfig } from "@remix-run/dev";
 
 const config: AppConfig = {
   appDirectory: "app",
-  tailwind: true,
-  postcss: true,
+  tailwind:true,
+  postcss:true,
+  assetsBuildDirectory: "public/build",
+  publicPath: "/build/",
+  server: "@remix-run/cloudflare",
+  serverBuildPath: "build/server/index.js",
+  serverModuleFormat: "esm",
+  serverPlatform: "neutral",
 };
 
 export default config;
