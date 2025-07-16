367bbb28977142fc11d99da6f0450aaddec4a495
Rishi Mishra <114818980+Rishi-Mishra0704@users.noreply.github.com>
2025-07-16 22:58:34 +0530

    remove wrangler

--- ecommerce/remix.config.ts
+++ ecommerce/remix.config.ts
@@ -2,14 +2,12 @@ import type { AppConfig } from "@remix-run/dev";
 
 const config: AppConfig = {
   appDirectory: "app",
-  tailwind:true,
-  postcss:true,
   assetsBuildDirectory: "public/build",
   publicPath: "/build/",
-  server: "@remix-run/cloudflare",
-  serverBuildPath: "build/server/index.js",
   serverModuleFormat: "esm",
-  serverPlatform: "neutral",
+  serverPlatform: "node",
+  tailwind:true,
+  postcss:true
 };
 
 export default config;
